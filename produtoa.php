a6dbb31b2510fc9979a8d78a0f02889e99b1f220
siriani <allan@fatecpomepeia.edu.br>
2021-05-14 16:45:56 -0300

    teste1

--- produtoa.php
+++ produtoa.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Aula de PHP</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- Third party plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
 <a href="index.php"> Ir para o inicio </a> </br>
 <a href="http://www.uol.com.br"> Site do UOL </a>
 
@@ -8,4 +29,5 @@
 <p> Aqui eu tenho um texto grande que posso </br>escrever infinitamente.</p>
 </br>
 <img src="pexels-valeriia-miller-6207287.jpg">
-
+</body>
+</html>
