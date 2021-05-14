ba18a4999fb0e73b57fc5c4022fda8d3fc6c6276
siriani <allan@fatecpomepeia.edu.br>
2021-05-14 16:57:54 -0300

    teste

--- produtoa.php
+++ produtoa.php
@@ -20,64 +20,86 @@
     </head>
     <body id="page-top">
 
-
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
   </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
-</div>
-<div class="card" style="width: 18rem;">
-  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Meu produto</h5>
-    <p class="card-text">Café arábica puro</p>
-    <a href="#" class="btn btn-primary">Comprar agora</a>
-  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
+<div class="container">
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src="pexels-valeriia-miller-6207287.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Meu produto</h5>
+    <p class="card-text">Café arábica puro</p>
+    <a href="#" class="btn btn-primary">Comprar agora</a>
+  </div>
+</div>
+</div>
 </br>
 </body>
 </html>
