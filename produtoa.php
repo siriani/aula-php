2c2af4e710760c678579d58311ff44dc3cb21898
siriani <allan@fatecpomepeia.edu.br>
2021-05-14 16:44:11 -0300

    teste1

--- produtoa.php
+++ produtoa.php
@@ -1,3 +1,11 @@
-<a href="index.php"> Ir para o inicio </a>
+<a href="index.php"> Ir para o inicio </a> </br>
 <a href="http://www.uol.com.br"> Site do UOL </a>
+
+<h1> Olá pessoas </h1>
+<h2> Olá pessoas </h2>
+
+
+<p> Aqui eu tenho um texto grande que posso </br>escrever infinitamente.</p>
+</br>
+<img src="pexels-valeriia-miller-6207287.jpg">
 
